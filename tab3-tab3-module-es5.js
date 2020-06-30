f11aade83cf13e9abea775a0e6753abc1a35023a
phancuong1234 <pvtho1111@gmail.com>
2020-07-01 00:13:05 +0700

    update

---
+++ tab3-tab3-module-es5.js
@@ -0,0 +1,320 @@
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["tab3-tab3-module"], {
+  /***/
+  "./node_modules/raw-loader/dist/cjs.js!./src/app/tab3/tab3.page.html":
+  /*!***************************************************************************!*\
+    !*** ./node_modules/raw-loader/dist/cjs.js!./src/app/tab3/tab3.page.html ***!
+    \***************************************************************************/
+
+  /*! exports provided: default */
+
+  /***/
+  function node_modulesRawLoaderDistCjsJsSrcAppTab3Tab3PageHtml(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony default export */
+
+
+    __webpack_exports__["default"] = "<ion-header [translucent]=\"true\">\n  <ion-toolbar>\n    <ion-title>\n      Tab 3\n    </ion-title>\n  </ion-toolbar>\n</ion-header>\n\n<ion-content [fullscreen]=\"true\">\n  <ion-header collapse=\"condense\">\n    <ion-toolbar>\n      <ion-title size=\"large\">Tab 3</ion-title>\n    </ion-toolbar>\n  </ion-header>\n\n  <app-explore-container name=\"Tab 3 page\"></app-explore-container>\n</ion-content>\n";
+    /***/
+  },
+
+  /***/
+  "./src/app/tab3/tab3-routing.module.ts":
+  /*!*********************************************!*\
+    !*** ./src/app/tab3/tab3-routing.module.ts ***!
+    \*********************************************/
+
+  /*! exports provided: Tab3PageRoutingModule */
+
+  /***/
+  function srcAppTab3Tab3RoutingModuleTs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony export (binding) */
+
+
+    __webpack_require__.d(__webpack_exports__, "Tab3PageRoutingModule", function () {
+      return Tab3PageRoutingModule;
+    });
+    /* harmony import */
+
+
+    var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+    /*! tslib */
+    "./node_modules/tslib/tslib.es6.js");
+    /* harmony import */
+
+
+    var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+    /*! @angular/core */
+    "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+    /* harmony import */
+
+
+    var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(
+    /*! @angular/router */
+    "./node_modules/@angular/router/__ivy_ngcc__/fesm2015/router.js");
+    /* harmony import */
+
+
+    var _tab3_page__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(
+    /*! ./tab3.page */
+    "./src/app/tab3/tab3.page.ts");
+
+    var routes = [{
+      path: '',
+      component: _tab3_page__WEBPACK_IMPORTED_MODULE_3__["Tab3Page"]
+    }];
+
+    var Tab3PageRoutingModule = function Tab3PageRoutingModule() {
+      _classCallCheck(this, Tab3PageRoutingModule);
+    };
+
+    Tab3PageRoutingModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+      imports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forChild(routes)],
+      exports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"]]
+    })], Tab3PageRoutingModule);
+    /***/
+  },
+
+  /***/
+  "./src/app/tab3/tab3.module.ts":
+  /*!*************************************!*\
+    !*** ./src/app/tab3/tab3.module.ts ***!
+    \*************************************/
+
+  /*! exports provided: Tab3PageModule */
+
+  /***/
+  function srcAppTab3Tab3ModuleTs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony export (binding) */
+
+
+    __webpack_require__.d(__webpack_exports__, "Tab3PageModule", function () {
+      return Tab3PageModule;
+    });
+    /* harmony import */
+
+
+    var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+    /*! tslib */
+    "./node_modules/tslib/tslib.es6.js");
+    /* harmony import */
+
+
+    var _ionic_angular__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+    /*! @ionic/angular */
+    "./node_modules/@ionic/angular/__ivy_ngcc__/fesm2015/ionic-angular.js");
+    /* harmony import */
+
+
+    var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(
+    /*! @angular/router */
+    "./node_modules/@angular/router/__ivy_ngcc__/fesm2015/router.js");
+    /* harmony import */
+
+
+    var _angular_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(
+    /*! @angular/core */
+    "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+    /* harmony import */
+
+
+    var _angular_common__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(
+    /*! @angular/common */
+    "./node_modules/@angular/common/__ivy_ngcc__/fesm2015/common.js");
+    /* harmony import */
+
+
+    var _angular_forms__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(
+    /*! @angular/forms */
+    "./node_modules/@angular/forms/__ivy_ngcc__/fesm2015/forms.js");
+    /* harmony import */
+
+
+    var _tab3_page__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(
+    /*! ./tab3.page */
+    "./src/app/tab3/tab3.page.ts");
+    /* harmony import */
+
+
+    var _explore_container_explore_container_module__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(
+    /*! ../explore-container/explore-container.module */
+    "./src/app/explore-container/explore-container.module.ts");
+    /* harmony import */
+
+
+    var _tab3_routing_module__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(
+    /*! ./tab3-routing.module */
+    "./src/app/tab3/tab3-routing.module.ts");
+
+    var Tab3PageModule = function Tab3PageModule() {
+      _classCallCheck(this, Tab3PageModule);
+    };
+
+    Tab3PageModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([Object(_angular_core__WEBPACK_IMPORTED_MODULE_3__["NgModule"])({
+      imports: [_ionic_angular__WEBPACK_IMPORTED_MODULE_1__["IonicModule"], _angular_common__WEBPACK_IMPORTED_MODULE_4__["CommonModule"], _angular_forms__WEBPACK_IMPORTED_MODULE_5__["FormsModule"], _explore_container_explore_container_module__WEBPACK_IMPORTED_MODULE_7__["ExploreContainerComponentModule"], _angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forChild([{
+        path: '',
+        component: _tab3_page__WEBPACK_IMPORTED_MODULE_6__["Tab3Page"]
+      }]), _tab3_routing_module__WEBPACK_IMPORTED_MODULE_8__["Tab3PageRoutingModule"]],
+      declarations: [_tab3_page__WEBPACK_IMPORTED_MODULE_6__["Tab3Page"]]
+    })], Tab3PageModule);
+    /***/
+  },
+
+  /***/
+  "./src/app/tab3/tab3.page.scss":
+  /*!*************************************!*\
+    !*** ./src/app/tab3/tab3.page.scss ***!
+    \*************************************/
+
+  /*! exports provided: default */
+
+  /***/
+  function srcAppTab3Tab3PageScss(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony default export */
+
+
+    __webpack_exports__["default"] = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL3RhYjMvdGFiMy5wYWdlLnNjc3MifQ== */";
+    /***/
+  },
+
+  /***/
+  "./src/app/tab3/tab3.page.ts":
+  /*!***********************************!*\
+    !*** ./src/app/tab3/tab3.page.ts ***!
+    \***********************************/
+
+  /*! exports provided: Tab3Page */
+
+  /***/
+  function srcAppTab3Tab3PageTs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony export (binding) */
+
+
+    __webpack_require__.d(__webpack_exports__, "Tab3Page", function () {
+      return Tab3Page;
+    });
+    /* harmony import */
+
+
+    var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+    /*! tslib */
+    "./node_modules/tslib/tslib.es6.js");
+    /* harmony import */
+
+
+    var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+    /*! @angular/core */
+    "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+    /* harmony import */
+
+
+    var _ionic_angular__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(
+    /*! @ionic/angular */
+    "./node_modules/@ionic/angular/__ivy_ngcc__/fesm2015/ionic-angular.js");
+
+    var Tab3Page = /*#__PURE__*/function () {
+      function Tab3Page(alertCtrl) {
+        _classCallCheck(this, Tab3Page);
+
+        this.alertCtrl = alertCtrl;
+      }
+
+      _createClass(Tab3Page, [{
+        key: "ionViewWillEnter",
+        value: function ionViewWillEnter() {
+          return Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__awaiter"])(this, void 0, void 0, /*#__PURE__*/regeneratorRuntime.mark(function _callee2() {
+            var _this = this;
+
+            var alert;
+            return regeneratorRuntime.wrap(function _callee2$(_context2) {
+              while (1) {
+                switch (_context2.prev = _context2.next) {
+                  case 0:
+                    if (!(localStorage.getItem("version") != null || localStorage.getItem("version") != undefined)) {
+                      _context2.next = 7;
+                      break;
+                    }
+
+                    _context2.next = 3;
+                    return this.alertCtrl.create({
+                      header: "Low battery",
+                      message: 'co version moi',
+                      subHeader: '10% of battery remaining',
+                      buttons: ['Dismiss']
+                    });
+
+                  case 3:
+                    alert = _context2.sent;
+                    _context2.next = 6;
+                    return alert.present();
+
+                  case 6:
+                    setTimeout(function () {
+                      return Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__awaiter"])(_this, void 0, void 0, /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+                        return regeneratorRuntime.wrap(function _callee$(_context) {
+                          while (1) {
+                            switch (_context.prev = _context.next) {
+                              case 0:
+                                _context.next = 2;
+                                return alert.dismiss();
+
+                              case 2:
+                              case "end":
+                                return _context.stop();
+                            }
+                          }
+                        }, _callee);
+                      }));
+                    }, 5000);
+
+                  case 7:
+                  case "end":
+                    return _context2.stop();
+                }
+              }
+            }, _callee2, this);
+          }));
+        }
+      }]);
+
+      return Tab3Page;
+    }();
+
+    Tab3Page.ctorParameters = function () {
+      return [{
+        type: _ionic_angular__WEBPACK_IMPORTED_MODULE_2__["AlertController"]
+      }];
+    };
+
+    Tab3Page = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+      selector: 'app-tab3',
+      template: Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"])(__webpack_require__(
+      /*! raw-loader!./tab3.page.html */
+      "./node_modules/raw-loader/dist/cjs.js!./src/app/tab3/tab3.page.html"))["default"],
+      styles: [Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"])(__webpack_require__(
+      /*! ./tab3.page.scss */
+      "./src/app/tab3/tab3.page.scss"))["default"]]
+    })], Tab3Page);
+    /***/
+  }
+}]);
+//# sourceMappingURL=tab3-tab3-module-es5.js.map
